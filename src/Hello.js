1e8fa6cf1e1be3151056850bb29acdc4a764cd3c
ashishrath619 <75829394+ashishrath619@users.noreply.github.com>
2021-04-04 23:17:38 +0530

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-expressions
+import React from "react";
+
+export default function Hello() {
+  return (
+    <div>
+      <h2>hello test page</h2>
+    </div>
+  );
+}
